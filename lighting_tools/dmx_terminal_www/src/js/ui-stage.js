c77d419e10bae0dd186e16b2829872fa100b94c2
Alan Meekins <alan.meekins@gmail.com>
2014-11-14 19:20:44 -0800

    Trimmed the branch to only the ola_ros sub directory

--- lighting_tools/dmx_terminal_www/src/js/ui-stage.js
+++
@@ -1,9 +0,0 @@
-//var Ui = {};
-
-Ui.StageView = function(options){
-  this.container = options.container;
-
-  options.container = this.container.id;
-  this.stage = new Kinetic.Stage(options);
-
-}
